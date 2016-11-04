4691421d5a94ad3d532a5b26281d4a297bf38317
Johnny Bai <johnny.bai91@gmail.com>
2016-11-04 11:51:40 -0700

    modified some things

--- EDA.py
+++ EDA.py
@@ -17,28 +17,34 @@ place_id        Check in location
 """
 
 def main():
+    f = open('output.txt', 'w') # Output file
     pandas.set_option('display.float_format', lambda x: '%.3f' % x) # up to 3 decimal places
     matplotlib.interactive(False)
+
     dataPath = "/Users/Johnny/Documents/FBData/train.csv"
     picklePath = "/Users/Johnny/Documents/FBData/train.pkl"
-    # Step 1. Read data set using pandas
+
+    # Read data set using pandas
     if not os.path.isfile(picklePath):
-        data = pandas.read_csv("/Users/Johnny/Documents/FBData/train.csv")
+        data = pandas.read_csv(dataPath)
         data.to_pickle(picklePath)
     else:
-        data = pandas.read_pickle(picklePath)
-    # Using pickle allows for faster subsequent runs
+        data = pandas.read_pickle(picklePath) # Using pickle allows for faster subsequent runs
 
-    # print(data.head(10) # print the first 10 rows
-    print(data.describe()) # count, mean, std, min, quartiles, max
-
-    # data.boxplot(column='accuracy').plot() # box plot for accuracy
+    # Get initial description of data: count, mean, std, min, quartiles, max
+    s = data.describe().to_string()
+    print(s)
+    f.write(s)
+    # Get counts for how many check ins a place_id has
     place_id = data['place_id'] # Place_id is our class/label; it contains a wide range of counts
-    #place_id.hist(bins=50).plot()
     place_id_freq = place_id.value_counts()
-    # Generates a histogram: x axis is the number of check-ins, y is how many place_ids have that number of check-ins
     print("     place_id_freq")
-    print(place_id_freq.describe())
+    f.write("\n     place_id_freq\n")
+    s = place_id_freq.describe().to_string()
+    print(s)
+    f.write(s)
+
+    # Generates a histogram: x axis is the number of check-ins, y is how many place_ids have that number of check-ins
     plt.figure() # Make a new figure
     plt.hist(place_id_freq.values, bins='fd') # Histogram of the counts
     plt.xticks(numpy.arange(0, 1900, 100))
@@ -46,6 +52,8 @@ def main():
     plt.ylabel("Number of place_ids")
     plt.savefig('Figures/Check_in_count.png')
 
+
+    f.close() # Close output.txt
 
 if __name__ == "__main__":
     main()
